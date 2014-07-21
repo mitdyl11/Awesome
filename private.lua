4e22f62083ad0a6e3a40ee2b2792fff9f228cb26
Mitdyl11 <Mitdyl11@hotmail.com>
2014-07-21 12:23:46 +0800

    Initial Commit

---
+++ private.lua
@@ -0,0 +1,11 @@
+local private = {}
+
+private.user = { name = "unlogic",
+                 city = "Gjovik",
+                 country = "Norway",
+                 time_offset = 1,
+                 loc =   { lat = 60.8, lon = 10.7 } }
+
+private.weather = { api_key = "sdccyyj9qax6atxuevd238tu" }
+
+return private
